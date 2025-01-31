d9ec8254391cf088c2ab3d67336cb3f4be5405e7
david-kasprak <kasprakdavid@gmail.com>
2025-02-01 00:10:57 +0200

    preparing structure (started)

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World! Im starting my NestJS exam';
+    return 'Hello World! Im starting my NestJS exam, and its now pushed to Github';
   }
 }
